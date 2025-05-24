5338be531f30061c1db11121b853c8c7141fb378
Mr. kuldeep <kkharoliya20@gmail.com>
2025-05-24 11:05:08 +0530

    ...

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -4,7 +4,7 @@ const asyncHandler = (fn) => async (req, res, next) => {
   } catch (error) {
     res.status(error.code || 500).json({
       success: false,
-      message: error.message || `Error occured in our application: ${error}`,
+      message: error?.message || `Error occured in our application: ${error}`,
     });
   }
 };
