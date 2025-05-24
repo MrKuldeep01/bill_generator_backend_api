1adaceab2ce9088bc9fe833dbf43f29e01b99dc0
Mr. kuldeep <kkharoliya20@gmail.com>
2025-05-24 10:57:39 +0530

    behaviour check

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,9 +1,10 @@
 import asyncHandler from "../utils/asyncHandler.js";
 const home = asyncHandler(async function (req, res) {
   console.log("working at home route controller:)");
-  res.status(200).json({
+  return res.status(200).json({
     success: true,
     message: "we are ready to grind ~!",
+    data: req
   });
 });
 
